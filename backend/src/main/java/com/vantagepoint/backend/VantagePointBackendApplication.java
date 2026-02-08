b9bda351960eed4d810a60743626cdca67476fb7
JuanEstebanGonzalez <juanchoes.gon@gmail.com>
2026-02-07 19:28:05 -0500

    proceso inicial estruturacion del docker y su configuracion

---
+++ backend/src/main/java/com/vantagepoint/backend/VantagePointBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vantagepoint.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VantagePointBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VantagePointBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vantagepoint/backend/VantagePointBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantagepoint.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VantagePointBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
