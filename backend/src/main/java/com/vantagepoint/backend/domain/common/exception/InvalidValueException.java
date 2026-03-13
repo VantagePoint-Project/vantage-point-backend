c90c773a704dbdf6cfbceaca01f59d8fe4e6313f
stivinson <stivinson02@gmial.com>
2026-03-13 15:26:23 -0500

    TASK004: Se elimina import que no se utilizan

--- backend/src/main/java/com/vantagepoint/backend/domain/common/exception/InvalidValueException.java
+++ backend/src/main/java/com/vantagepoint/backend/domain/common/exception/InvalidValueException.java
@@ -1,7 +1,5 @@
 package com.vantagepoint.backend.domain.common.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class InvalidValueException extends DomainException {
     public InvalidValueException(String message) {
         super(message);
